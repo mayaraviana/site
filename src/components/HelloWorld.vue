142fe7b87a007363e2312c2f45f5f6ac9280f55d
Mayara Viana <54589016+mayaraviana@users.noreply.github.com>
2021-07-17 23:11:56 -0300

    vue starter project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <b-container>
+      <b-row>
+        <h1 class="home-msg">{{ msg }}</h1>
+      </b-row>
+      <b-row>
+        <b-col cols="12" md="4">col</b-col>
+        <b-col cols="6" md="4">col</b-col>
+        <b-col cols="6" md="4">col</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home-msg {
+  font-size: 2.5em;
+}
+@media (min-width: 900px) {
+  .home-msg {
+    font-size: 4em;
+  }
+}
+</style>
